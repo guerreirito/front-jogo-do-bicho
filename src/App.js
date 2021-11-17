724c9231079007763a6b7faa24959d26b29df2db
Fernando <f.luhis@gmail.com>
2021-11-17 00:06:00 -0300

    comit inicial

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+import "./App.css"
+
+function App() {
+
+  async function getData() {
+    const response = await axios.get("http://localhost:3333/");
+    const data = await response.data;
+    const root = document.querySelector("#root");
+    const body = document.querySelector("body");
+    root.innerHTML = data;
+    const tables = root.querySelectorAll("table");
+
+    root.innerHTML = ""
+
+    tables.forEach(item => {
+      root.appendChild(item)
+    })
+
+    body.appendChild(root)
+  }
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  return <></>;
+}
+
+export default App;
